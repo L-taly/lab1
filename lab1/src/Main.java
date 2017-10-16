a8f4cb16432e06ec2d6834871d75ed6462395950
L-taly <litanlinlitanlin@outlook.com>
2017-10-16 16:43:23 +0800

    add c4

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -106,6 +106,7 @@ public class Main {
                             }
                         }
                     }
+                    //////////
                     catch (IOException e)
                     {
                         e.getStackTrace();
